b5dfb067224013c6e72d96a0b35850fc0d893bbe
PieroCruzado19 <piero.cruzado@tecsup.edu.pe>
2025-05-18 20:24:54 -0500

    👍
    :)

--- crud_producto/wsgi.py
+++ crud_producto/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for crud_producto project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Añadir la ruta de tu proyecto
+path = '/home/pierocruzado/crud_producto'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud_producto.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
